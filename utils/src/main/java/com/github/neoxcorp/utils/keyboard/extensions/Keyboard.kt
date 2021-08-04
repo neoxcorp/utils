24da4858f3a9fd88da16c14a263187ba1cf7c521
Andrew Ilchenko <a4etko@gmail.com>
2021-08-04 17:11:32 +0300

    add extension for fragment activity

--- utils/src/main/java/com/github/neoxcorp/utils/keyboard/extensions/Keyboard.kt
+++ utils/src/main/java/com/github/neoxcorp/utils/keyboard/extensions/Keyboard.kt
@@ -2,12 +2,19 @@ package com.github.neoxcorp.utils.keyboard.extensions
 
 import android.app.Activity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.LifecycleOwner
 import com.github.neoxcorp.utils.keyboard.KeyboardVisibilityEvent
 
 fun Activity?.keyboardListener(owner: LifecycleOwner, isOpenKeyboard: (Boolean) -> Unit) {
     this?.let { activity ->
         KeyboardVisibilityEvent.setEventListener(activity, owner, { isOpenKeyboard.invoke(it) })
+    }
+}
+
+fun FragmentActivity?.keyboardListener(isOpenKeyboard: (Boolean) -> Unit) {
+    this?.let { activity ->
+        KeyboardVisibilityEvent.setEventListener(activity, activity, { isOpenKeyboard.invoke(it) })
     }
 }
 
